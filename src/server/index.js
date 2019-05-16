660e76f89509b32259f060a437c7de83a46fc515
Austinlp4 <apendergrast44@gmail.com>
2019-05-16 09:39:00 -0500

    comments

--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,12 @@ const graphqlHTTP = require('express-graphql');
 const gql = require('graphql-tag');
 const { buildASTSchema } = require('graphql');
 
+// mock data
 const POSTS = [
     { author: "Austin Pendergrast", body: "Hello World" },
     { author: "Sydney Silva", body: "Hi There" },
 ]
-
+// parses the string and converts it to GraphQL AST
 const schema = buildASTSchema(gql`
     type Query {
         posts: [Post]
@@ -22,6 +23,7 @@ const schema = buildASTSchema(gql`
     }
 `);
 
+// resolvers that tell graphql how to query the data
 const mapPost = (post, id) => post && ({ id, ...post });
 
 const root = {
@@ -31,6 +33,8 @@ const root = {
 
 const app = express();
 app.use(cors());
+//The graphqlHTTP function creates an Express server running GraphQL,
+// which expects the resolvers as rootValue
 app.use('/graphql', graphqlHTTP({
     schema,
     rootValue: root,
